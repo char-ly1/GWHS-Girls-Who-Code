3822924a13f2e4a7766af1db997f91aea85439f9
Maggie <maferrer2@s.sfusd.edu>
2025-01-09 17:00:08 -0800

    Added the method and fixed some bugs, still needs to be double checked and tested

--- minesweeper.py
+++ minesweeper.py
@@ -1,1 +1,24 @@
 import pygame
+
+def findSurroundingBombs(arr, row, col):
+    life = 0 
+
+    if( row >0 and arr[row -1][col] == 1):
+        life += 1
+    if( row < arr.length -1 and arr[row +1][col] == 1):
+        life += 1
+    if( col < arr[0].length -1 and arr[row][col +1] == 1):
+        life += 1
+    if( col > 0 and arr[row][col -1] == 1):
+        life += 1
+    if ( (col > 0 and row >0) and arr[row -1][col-1] == 1 ):
+        life += 1
+    if ( (col > 0 and row < arr.length-1) and arr[row +1][col-1] == 1 ):
+        life += 1
+    if ( (col < arr[0].length -1 and row < arr.length-1) and arr[row +1][col+1] == 1 ):
+        life +=1
+    if ( (col < arr[0].length -1 and row >0) and arr[row - 1][col+1] == 1):
+        life += 1
+        
+    return life 
+
