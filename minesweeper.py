18fa0645b06d847dc5a507c7eb3fc3b2399e3051
Nicole <Niliang1@s.sfusd.edu>
2025-03-06 16:59:16 -0800

    rave mode

--- minesweeper.py
+++ minesweeper.py
@@ -1,29 +1,49 @@
 import pygame
 import time
+import sys
 
 WINDOW_HEIGHT = 400
 WINDOW_WIDTH = 400
+BLACK = (0, 0, 0)
+WHITE = (200, 0, 0)
 
-def CreateBoard(BOARDSIZE):
+
+# def CreateBoard(BOARDSIZE):
+#     pygame.init()
+#     screen = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
+#     running = True
+#     while running:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#              running = False
+#             if event.type == pygame.MOUSEBUTTONUP:
+#                 print(pygame.mouse.get_pos())
+#         screen.fill("white")
+#         pygame.display.flip()
+def main():
+    global SCREEN, CLOCK
     pygame.init()
-    screen = pygame.display.set_mode((650, 650))
-    running = True
-    while running:
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill(WHITE)
+    pygame.display.flip()
+
+    while True:
+        drawGrid()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-             running = False
-            if event.type == pygame.MOUSEBUTTONUP:
-                print(pos = pygame.mouse.get_pos())
-        screen.fill("white")
-        pygame.display.flip()
+                pygame.quit()
+                sys.exit()
+
+       
 
 def drawGrid():
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     blockSize = 20 #Set the size of the grid block
     for x in range(0, WINDOW_WIDTH, blockSize):
         for y in range(0, WINDOW_HEIGHT, blockSize):
             rect = pygame.Rect(x, y, blockSize, blockSize)
-            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+            # pygame.draw.rect(SCREEN, WHITE , rect, 1)
 
-CreateBoard(5)
-
- 
+# CreateBoard(5)
+main()
